8e8f04181125be1b93f59ac1394b5fd525ba8635
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-09-23 13:34:35 +0200

    Project: interactive-card-details-form-main V1

---
+++ interactive-card-details-form-main/card-details/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "Secondary": "hsl(270, 3%, 80%)",
+        "Secondary-darker": "hsl(279, 6%, 70%)",
+        "Primary": "hsl(278, 68%, 11%)",
+
+        "Error": "hsl(0, 100%, 66%)",
+        "Gradient-start": "hsl(249, 99%, 64%)",
+        "Gradient-end": "hsl(278, 94%, 30%)",
+      },
+      fontFamily: {
+        "font-family": "Space Grotesk, sans-serif",
+      },
+      fontSize: {
+        "regular-size": "1.125rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
